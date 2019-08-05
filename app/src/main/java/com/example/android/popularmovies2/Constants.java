4608997230301db61efcc997467e3aa91354e396
Yahia Salem <yahiah.salem@gmail.com>
2019-08-05 17:27:11 +0200

    Changing constants

--- app/src/main/java/com/example/android/popularmovies2/Constants.java
+++ app/src/main/java/com/example/android/popularmovies2/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
 
     public static final String IMAGE_URL = "http://image.tmdb.org/t/p/w185/";
 
-    public static final String apiKey = "24c7f9a49cde18c8e2e2afdf4cb7330a";
+    public static final String apiKey = "";
 
     public static final String POPULAR_MOVIES =
             "http://api.themoviedb.org/3/movie/popular?api_key=" + apiKey;
